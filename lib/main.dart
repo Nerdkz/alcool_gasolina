a2160590de5bef3bf0dfe65bb6ba79de5206faff
Nerdkz <viniciusmendonca@gec.inatel.br>
2020-04-12 12:57:35 -0300

    firt commit of source code

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:alcoolgasolina/Home.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+      MaterialApp(
+        home: Home(),
+        debugShowCheckedModeBanner: false,
+      )
+  );
+}
